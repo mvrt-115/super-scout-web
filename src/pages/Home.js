cd19204f5e702fe15e3d9c65dae85451a9edf443
Gavin Hung <gavinkhung@gmail.com>
2021-01-03 00:36:16 -0800

    added sample data

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,19 +7,66 @@ function Home() {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const regional = 'LAN';
-        const match = '79';
-        const alliance = 'red';
-        const team = '115';
+        // what data from the qr code will look like
         const data = {
-          name: 'hello',
+          data: {
+            matchNum: 10,
+            teamNum: 31,
+            startLocations: 'true,false,false',
+            initLineCrosses: true,
+            preloads: 6,
+            autonUpper: 7,
+            autonInner: 3,
+            autonUpperMissed: 5,
+            autonBottom: 8,
+            autonBottomMissed: 2,
+            teleopUpper: 2,
+            teleopInner: 0,
+            teleopUpperMissed: 1,
+            teleopBottom: 12,
+            teleopBottomMissed: 6,
+            trench: true,
+            defense: true,
+            rotation: false,
+            position: true,
+            stuck: true,
+            disabled: true,
+            cycles: 16,
+            climbTime: 2495,
+            hangFail: false,
+            levelFail: false,
+            attemptHang: true,
+            attemptLevel: true,
+            buddy: false,
+            comments: '',
+            minfo: '31@CASF:b[31,75,95]',
+            sctid: 0,
+          },
+          verif: 9613,
         };
+
+        const minfo = data['data']['minfo'];
+
+        const regional = minfo.substring(
+          minfo.indexOf('@') + 1,
+          minfo.indexOf(':')
+        );
+        const match = data['data']['matchNum'] + '';
+        const isBlueAlliance =
+          minfo.substring(minfo.indexOf(':') + 1, minfo.indexOf('[')) === 'b';
+        const alliance = isBlueAlliance ? 'blue' : 'red';
+        const team = data['data']['teamNum'] + '';
+
+        console.log(regional, match, alliance, team);
+
         await db
-          .collection(regional)
+          .collection('regional')
+          .doc(regional)
+          .collection('matches')
           .doc(match)
           .collection(alliance)
           .doc(team)
-          .set(data);
+          .set(data['data']);
       } catch (e) {
         console.log(e);
       }
